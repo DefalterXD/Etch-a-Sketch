cb6d03eb4613a58568ddbab3880e6a9d8c594539
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-01-26 19:45:42 +0500

    feat: add a function for removing existing cells in the field

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ const inputFieldSize = document.querySelector('input[type="range"]');
 const makeField = function makeFieldOfCells() {
     let gridValue = Number(inputFieldSize.value);
     gridText(gridValue);
+    const gridFieldCells = document.querySelectorAll('.cell');
+    checkForCells(gridFieldCells);
 };
 
 const gridText = function countHowMuchCellsInGrid(gridValue = 10) {
     gridSizeCounter.textContent = `Grid: ${gridValue}x${gridValue}`;
+};
+
+const checkForCells = function checkForExistingCells(nodeOfCells) {
+    nodeOfCells.forEach((cell) => {
+        if (cell) {
+            gridField.removeChild(cell);
+        }
+    });
 };
