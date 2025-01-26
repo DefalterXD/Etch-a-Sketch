4fe6229847108f018402e9b742bc2077e557dca4
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-01-26 19:41:14 +0500

    feat: add a function to calculate and view size of the grid

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const gridField = document.querySelector('.grid__field');
 const inputFieldSize = document.querySelector('input[type="range"]');
 
 const makeField = function makeFieldOfCells() {
-    
+    let gridValue = Number(inputFieldSize.value);
+    gridText(gridValue);
+};
+
+const gridText = function countHowMuchCellsInGrid(gridValue = 10) {
+    gridSizeCounter.textContent = `Grid: ${gridValue}x${gridValue}`;
 };
