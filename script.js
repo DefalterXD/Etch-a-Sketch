3ddf7d1e2f617e928eb4839ddcc0438ee0acf81b
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-01-26 19:39:47 +0500

    feat: add a function to fill out the field

---
+++ script.js
@@ -0,0 +1,7 @@
+const gridSizeCounter = document.querySelector('.size__view');
+const gridField = document.querySelector('.grid__field');
+const inputFieldSize = document.querySelector('input[type="range"]');
+
+const makeField = function makeFieldOfCells() {
+    
+};
